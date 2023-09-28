ece80762f95ec7fbb4c71a4da4bfb7284f1461d1
Pranav Rajbhandari <pranavrajbhandari123@gmail.com>
2023-09-28 11:03:40 +0545

    Typescript and vue

---
+++ App.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="app">
+    <header>
+      <div class="title">
+        <img src="./assets/heart.svg" alt="site logo" />
+        <h1>City</h1>
+      </div>
+      <div class="order">
+        <button @click="handleClick('title')">Order by title</button>
+        <button @click="handleClick('salary')">Order by salary</button>
+        <button @click="handleClick('location')">Order by location</button>
+      </div>
+    </header>
+
+    <JobList :jobs="jobs" :order="order" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref, toRef, toRefs } from "vue";
+import Job from "./types/job";
+import OrderTerm from "./types/OrderTerm";
+import JobList from "./components/JobList.vue";
+
+export default defineComponent({
+  name: "App",
+  components: { JobList },
+  setup() {
+    // const state = reactive({
+    //   name: 'Pranav',
+    //   age: 21 as string | number
+    //)}
+    //return{...toRefs(state)}
+    // ---------------------------------------ref-------------------
+    // const name = ref('pranav')
+    // const age = ref<number | string>(21)
+
+    //   return {name, age}
+    const jobs = ref<Job[]>([
+      { title: "Nepal", location: "ktm", salary: 30000, id: "1" },
+      {
+        title: "quarryman",
+        location: "death mountain",
+        salary: 40000,
+        id: "2",
+      },
+      {
+        title: "flute player",
+        location: "the lost woods",
+        salary: 35000,
+        id: "3",
+      },
+      { title: "fisherman", location: "lake hylia", salary: 21000, id: "4" },
+      {
+        title: "prison guard",
+        location: "gerudo valley",
+        salary: 32000,
+        id: "5",
+      },
+    ]);
+    const order = ref<OrderTerm>("title");
+
+    const handleClick = (term: OrderTerm) => {
+      order.value = term;
+    };
+    return { jobs, handleClick, order };
+  },
+  // methods: {
+  //   changeName(name:string){
+  //     this.name = name
+
+  //   },
+  //   changeAge(age:number | string){
+  //     this.age = age
+
+  //   }
+  // }
+});
+</script>
+
+<style>
+header {
+  text-align: center;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+header .title {
+  display: flex;
+  justify-content: center;
+}
+header img {
+  width: 60px;
+  margin-right: 20px;
+}
+header h1 {
+  font-size: 3em;
+}
+</style>
